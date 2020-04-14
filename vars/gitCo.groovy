bab2bec320e7ce6f7140d54c82c4cd726bdbda0d
samer-habash <sam.habash1984@gmail.com>
2020-04-14 16:42:57 +0300

    asd

--- vars/gitCo.groovy
+++ vars/gitCo.groovy
@@ -9,28 +9,28 @@ def call(Map gitcoParams) {
   }
 
 
-// Checkout git repo for maven
-@Library('general-jenkins-library@master') _
-
-pipeline {
-    agent any
-    stages {
-        stage('Git Checkout Maven') {
-            steps {
-            gitCo(
-                branch: "master",
-                url: "https://github.com/web3j/sample-project-maven"
-            )}
-         stage('Build Maven') {
-            PodTemplateMaven('general-pod-label-cluster1')
-                node('general-pod-label-cluster1') {
-        		    stage('Build Maven Release') {
-        		        container('maven-image') {
-        		            sh "mvn clean install"
-        		        }
-        			}
-        		}
-        	}
-        }
-    }
-}
+// // Checkout git repo for maven
+// @Library('general-jenkins-library@master') _
+//
+// pipeline {
+//     agent any
+//     stages {
+//         stage('Git Checkout Maven') {
+//             steps {
+//             gitCo(
+//                 branch: "master",
+//                 url: "https://github.com/web3j/sample-project-maven"
+//             )}
+//          stage('Build Maven') {
+//             PodTemplateMaven('general-pod-label-cluster1')
+//                 node('general-pod-label-cluster1') {
+//         		    stage('Build Maven Release') {
+//         		        container('maven-image') {
+//         		            sh "mvn clean install"
+//         		        }
+//         			}
+//         		}
+//         	}
+//         }
+//     }
+// }
