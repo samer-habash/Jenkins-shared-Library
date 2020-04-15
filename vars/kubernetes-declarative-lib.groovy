0b0c9c9fff6757b0a963e47ed6512b05d5453bba
samer-habash <sam.habash1984@gmail.com>
2020-04-15 10:48:22 +0300

    Change in kubernetes-declarative-lib.groovy

--- vars/kubernetes-declarative-lib.groovy
+++ vars/kubernetes-declarative-lib.groovy
@@ -10,7 +10,40 @@ def call(Map kubernetesParams, code) {
 				image: kubernetesParams.image,
 				command: 'cat',
 				ttyEnabled: 'true')
-		]) {
-            code()
-        }
+		])
 	}
+
+// // Apply on local Jenkins pipeline Job
+// @Library('general-jenkins-library@master') _
+// pipeline {
+//     agent {
+//         kubernetes-declarative-lib(
+//             cloud 'kubernetes-Cluster2'
+//         	label 'general-pod-label-cluster2'
+//         	activeDeadlineSeconds '300'
+//         	containerTemplate {
+//                 name 'maven-image'
+//                 image 'maven:3.3.9-jdk-8-alpine'
+//         	}
+//         }
+//     }
+//     stages {
+//         stage('clone git maven') {
+//             steps {
+//                 container('maven-image') {
+// 			        gitCo(
+// 			            branch: "master",
+// 			            url: "https://github.com/web3j/sample-project-maven.git"
+// 			        )}
+//                 }
+//             }
+//         }
+// 	    stage('Install Maven') {
+// 	        steps {
+// 		        container('maven-image') {
+// 			        sh 'mvn clean install'
+// 			    }
+// 		    }
+//         }
+//     }
+// }
